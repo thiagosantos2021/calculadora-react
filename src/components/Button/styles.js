9496f7418dbbd7537d4a29026a8c792c4a4bddfb
Thiago Santos <thiago_santos_1990@hotmail.com>
2023-04-02 17:55:18 -0300

    prototipo de calculadora react

---
+++ src/components/Button/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+    margin-top: 5px;
+    padding: 20px;
+    border: 1px solid #CDCDCD;
+    background-color: #00AAFF;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+
+    &:hover {
+        opacity: 0.6;
+    }
+`
